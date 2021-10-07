ef3bf58a618476c88f042e99cc37e662b0250808
Cullin Flynn <cullinflynn@rocketmortgage.com>
2021-10-07 13:50:09 -0400

    committing Exponents Lab - first commit

---
+++ ExponentsLab/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ExponentsLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+
+            System.Console.WriteLine("Learn your squares and cubes!");
+            string name = GetInput("First of all, what's your name?");
+            Console.WriteLine($"Thanks, {name}!");
+
+            while (goOn == true)
+            {
+                string result = GetInput("Please input an integer:");
+                int intResult = int.Parse(result);
+                Console.WriteLine($"You entered the number {intResult}.");
+                Console.WriteLine($"Starting from 1 and proceeding up to your number {intResult}, we get the following:");
+                {
+                    Console.WriteLine("{0, -20} {1,-20} {2, -20}", "Number", "Squared", "Cubed");
+                    for (int i = 1; i <= intResult; i++)
+                    {
+                        Console.WriteLine("{0, -20} {1, -20} {2, -20}", i, i * i, i * i * i);
+                    }
+                }
+                goOn = Continue(name);
+            }
+
+        }
+        //Method 1: to get user input
+        public static string GetInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string output = Console.ReadLine();
+            return output;
+        }
+        //Continue Loop
+        
+        //Method 2: Calculate square based on user input 
+        public static int GetSquare(int i)
+        {
+            int square = (i * i);
+            return square;
+        }
+
+        //Method 3: Calculate cube based on user input
+        public static int GetCube(int i)
+        {
+            int cube = (i * i * i);
+            return cube;
+        }
+        
+        public static bool Continue(string name)
+        {
+            string continueAnswer = GetInput("Would you like to continue? Y/N");
+
+            if (continueAnswer == "y")
+            {
+                return true;
+            }
+            else if (continueAnswer == "n")
+            {
+                Console.WriteLine($"No problem, {name}. Enjoy the rest of your day!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, '{continueAnswer}' is not a valid response.");
+                Console.WriteLine("Please enter 'y' to continue or 'n' to exit.");
+                return Continue(name);
+            }
+        }
+    }
+}
